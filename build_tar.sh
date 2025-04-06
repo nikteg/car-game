6326a92202c8be5e2ca9efa67d87565ab36d04f3
Niklas <me@sodapop.se>
2025-04-06 20:32:22 +0200

    fixes

--- build_tar.sh
+++ build_tar.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Usage: ./build_tar.sh [output_file]
-# If no output file is specified, it defaults to ~/Desktop/motorcycle.tar.gz
+# If no output file is specified, it defaults to ~/Desktop/car-game.tar.gz
 
 set -euo pipefail  # Exit on error, unset variables, or pipeline failures.
 
-output_file="${1:-$HOME/Desktop/motorcycle.tar.gz}"
+output_file="${1:-$HOME/Desktop/car-game.tar.gz}"
 echo "Creating tarball..."
 tar --no-xattr --disable-copyfile -czvf "$output_file" Dockerfile .dockerignore public/
 echo "Tarball creation completed successfully. Output file: $output_file"
